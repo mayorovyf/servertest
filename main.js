a4b6e8cc5bf9a286ca8aa704ed3b6a46a6273a34
Anta <gus05522@gmail.com>
2024-10-12 15:05:37 +0300

    3

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ const port = 3000;
 // Middleware для парсинга JSON
 app.use(express.json());
 
+let sum=0;
+
 // Обработка POST-запроса
 app.post('/message', (req, res) => {
+    sum++;
     const message = req.body.message;
-    const responseMessage = `Ваш ответ: ${message}`;
+    const responseMessage = `Ваш 1: ${message}`+sum;
     res.json({ response: responseMessage });
 });
 
 app.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}`);
+    console.log(`Сервер запущен на http://ip:${port}`);
 });
    
